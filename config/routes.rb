a1ed16d492d7350910c8dd41e4881ee9019ce86b
Neil Gevaux <neilgevaux@gmail.com>
2022-01-22 14:12:06 +0000

    Added Devise for admin account

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  # get 'home/index'
   resources :feedbacks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root 'home#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
